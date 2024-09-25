1f2a6a207fad04664cda68e0e5bed1addcd7979f
Hyosun Kim <84787653+hy5sun@users.noreply.github.com>
2024-09-25 14:22:19 +0900

    initial commit

---
+++ src/main/java/com/example/mom_check/MomCheckApplication.java
@@ -0,0 +1,13 @@
+package com.example.mom_check;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MomCheckApplication.class, args);
+	}
+
+}
