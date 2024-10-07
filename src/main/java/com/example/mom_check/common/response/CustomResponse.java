d5aa5a7af7147507eb7a75cea07b571372919674
Hyosun Kim <84787653+hy5sun@users.noreply.github.com>
2024-10-07 16:42:47 +0900

    feat: Exception Handling

---
+++ src/main/java/com/example/mom_check/common/response/CustomResponse.java
@@ -0,0 +1,23 @@
+package com.example.mom_check.common.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatusCode;
+
+import java.util.ArrayList;
+
+@Getter
+@AllArgsConstructor
+public class CustomResponse {
+    private final int statusCode;
+    private final String message;
+    private final Object data;
+
+    public static CustomResponse response(final HttpStatusCode statusCode, final String message, final Object data) {
+        return new CustomResponse(statusCode.value(), message, data);
+    }
+
+    public static CustomResponse response(final HttpStatusCode statusCode, final String message) {
+        return new CustomResponse(statusCode.value(), message, new ArrayList<>());
+    }
+}
