da085f237b1aa5ec19cc760ea2d29509bc9329a5
Hyosun Kim <84787653+hy5sun@users.noreply.github.com>
2024-10-08 18:50:43 +0900

    feat: baby Domain 생성

---
+++ src/main/java/com/example/mom_check/baby/controller/BabyController.java
@@ -0,0 +1,12 @@
+package com.example.mom_check.baby.controller;
+
+import com.example.mom_check.baby.service.BabyService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/babies")
+@RequiredArgsConstructor
+public class BabyController {
+}
---
+++ src/main/java/com/example/mom_check/baby/service/BabyService.java
@@ -0,0 +1,9 @@
+package com.example.mom_check.baby.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BabyService {
+}
