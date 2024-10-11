661e7507669f3dbc1ef10f50a58531face3d4c44
Hyosun Kim <84787653+hy5sun@users.noreply.github.com>
2024-10-12 00:08:44 +0900

    feat: Diary Service & Controller 생성

---
+++ src/main/java/com/example/mom_check/diary/controller/DiaryController.java
@@ -0,0 +1,11 @@
+package com.example.mom_check.diary.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/diaries")
+@RequiredArgsConstructor
+public class DiaryController {
+}
---
+++ src/main/java/com/example/mom_check/diary/service/DiaryService.java
@@ -0,0 +1,17 @@
+package com.example.mom_check.diary.service;
+
+import com.example.mom_check.diary.domain.Diary;
+import com.example.mom_check.diary.dto.CreateDiaryRequest;
+import com.example.mom_check.diary.dto.DetailDiaryResponse;
+import com.example.mom_check.diary.repository.DiaryRepository;
+import com.example.mom_check.user.domain.User;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class DiaryService {
+}
