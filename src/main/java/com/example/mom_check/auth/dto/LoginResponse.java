0cfda5f96b63d4df00544e9f450ab19faaaa1e28
Hyosun Kim <84787653+hy5sun@users.noreply.github.com>
2024-10-27 22:46:44 +0900

    feat: response 수정

--- src/main/java/com/example/mom_check/auth/dto/LoginResponse.java
+++ src/main/java/com/example/mom_check/auth/dto/LoginResponse.java
@@ -1,17 +1,21 @@
 package com.example.mom_check.auth.dto;
 
 import com.example.mom_check.baby.domain.Baby;
+import com.example.mom_check.user.domain.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+import java.util.UUID;
 
 @Getter
 @AllArgsConstructor
 public class LoginResponse {
+    private UUID id;
     private String accessToken;
     private String babyName;
     private String expectedBirth;
 
-    public static LoginResponse toDto(String accessToken, Baby baby) {
-        return new LoginResponse(accessToken, baby.getName(), baby.getExpectedBirth());
+    public static LoginResponse toDto(User user, String accessToken, Baby baby) {
+        return new LoginResponse(user.getId(), accessToken, baby.getName(), baby.getExpectedBirth());
     }
 }
